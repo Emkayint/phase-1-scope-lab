dbffddfbe66c9b7470e1278c3034ea99ded5ad6f
Martin <mkmartinoes@gmail.com>
2022-04-09 15:02:46 +0300

    best consumer test

--- index.js
+++ index.js
@@ -15,3 +15,9 @@ function setBestCustomer(){
 function overwriteBestCustomer(){
     bestCustomer = "maybe bob";
 }
+
+
+const leastFavoriteCustomer = "Boobby"
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "Kaay";
+}
